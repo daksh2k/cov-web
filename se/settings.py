782397141277f0243e36b21f196cd91f94937dae
daksh2k <daksh2k@gmail.com>
2021-05-08 01:58:11 +0530

    mongo try

--- se/settings.py
+++ se/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +152,4 @@ MESSAGE_TAGS = {
 
 # Activate Django-Heroku.
 # django_heroku.settings(locals())
+django_heroku.settings(locals(), databases=False)
