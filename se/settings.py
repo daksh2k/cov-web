2ced6bbb6c32c291bd92922d60b5fae80fc78284
daksh2k <daksh2k@gmail.com>
2021-05-09 00:20:42 +0530

    Fix 500 error

--- se/settings.py
+++ se/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("django_secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cov-web.herokuapp.com']
 
